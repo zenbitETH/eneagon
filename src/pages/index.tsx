8028501c52fb0b3ec87267836bcc03831fb30f0e
daphnehdz <daphne.chernandez@gmail.com>
2022-09-08 20:19:02 -0500

    testing colors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,11 @@ import Header from '../components/header';
 
 const Home: FC = () => {
 	return (
-		<div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+		<div className="relative flex items-top justify-center min-h-screen 
+
+		bg-gradient-to-tl from-purr-500 via-blu-500 to-coral-500
+		
+		sm:items-center py-4 sm:pt-0">
 			
 			<div className="absolute top-6 right-6">
 				
