8460ddd0c5a9dd3c92a383714d1d5dbdb542bd44
daphnehdz <daphne.chernandez@gmail.com>
2022-09-08 20:57:38 -0500

    bg transparent header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export default function Header() {
     return (
-        <div className="fixed top-0 w-full bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+        <div className="fixed top-0 w-full border-gray-200 px-2 sm:px-4 py-2.5 rounded bg-transparent">
           <div className="container flex flex-wrap justify-between items-center mx-auto">
           <a href="/yarn-error.log" className="flex items-center">
 
@@ -20,15 +20,15 @@ export default function Header() {
             </button>
           </div>
           <div className="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="navbar-cta">
-            <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul className="flex flex-col p-4 mt-4 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 bg-transparent text-white">
               <li>
-                <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Projects</a>
+                <a href="#" className="block py-2 pr-4 pl-3 rounded hover:bg-gray-100 md:p-0">Projects</a>
               </li>
               <li>
-                <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Stakeholders </a>
+                <a href="#" className="block py-2 pr-4 pl-3 rounded hover:bg-gray-100 md:p-0">Stakeholders </a>
               </li>
               <li>
-                <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Docs</a>
+                <a href="#" className="block py-2 pr-4 pl-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:p-0 dark:border-gray-700">Docs</a>
               </li>
             </ul>
           </div>
