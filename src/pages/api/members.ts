b43e9922f52e71f042f9c7b45b96bc01bbf99662
Chisom Ikejiani <trillaboi@gmail.com>
2022-09-15 08:45:24 -0400

    added api functionality

---
+++ src/pages/api/members.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { Octokit } from "octokit"
+
+// List all users who are members of an organization. If the authenticated user is also a member of this organization then both concealed and public members will be returned.
+const octokit = new Octokit({
+    auth: process.env.NEXT_PUBLIC_OCTOKIT
+})
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    const result = await octokit.request("GET /orgs/{org}/members", {
+        org: "zenbitETH",
+    })
+
+    res.status(200).json( result )
+}
+
+export default handler
