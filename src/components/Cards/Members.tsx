cb42e947f2a7378690368b480e5f4b65c5c2d815
HabacucMX <habacuc.vera@gmail.com>
2022-09-18 16:00:59 -0500

    dashboard project distribution

--- src/components/Cards/Members.tsx
+++ src/components/Cards/Members.tsx
@@ -20,123 +20,14 @@ export default function Members() {
               <div className="memberName">Username / @username.xyz</div>
               <div className="memberRole">Role</div>
             </div>
-            <div className="memberPhoto">
-              <Image src={blank} height={50} width={50}/>
-            </div>
-            <div className="memberData">
-              <div className="memberName">Username / @username.xyz</div>
-              <div className="memberRole">Role</div>
-            </div>
-            <div className="memberPhoto">
-              <Image src={blank} height={50} width={50}/>
-            </div>
-            <div className="memberData">
-              <div className="memberName">Username / @username.xyz</div>
-              <div className="memberRole">Role</div>
-            </div>
-            <div className="memberPhoto">
-              <Image src={blank} height={50} width={50}/>
-            </div>
-            <div className="memberData">
-              <div className="memberName">Username / @username.xyz</div>
-              <div className="memberRole">Role</div>
-            </div>
-            <div className="memberPhoto">
-              <Image src={blank} height={50} width={50}/>
-            </div>
-            <div className="memberData">
-              <div className="memberName">Username / @username.xyz</div>
-              <div className="memberRole">Role</div>
-            </div>
-            <div className="memberPhoto">
-              <Image src={blank} height={50} width={50}/>
-            </div>
-            <div className="memberData">
-              <div className="memberName">Username / @username.xyz</div>
-              <div className="memberRole">Role</div>
-            </div>
           </div>
         </div>
       </div>
       <div className="teamMembers">
-        
-        
           <div className="">
           <h1 className="tabMembers ">Stakeholders</h1>
           <div className="memberGrid">
             <div className="col-span-2 font-bold text-center">1</div>
-            <div className="stakeholderPhoto">
-              <Image src={blank} height={40} width={40}/>
-            </div>
-            <div className="stakeholderData">
-              <div className="stakeholderName">Address / ENS / Lens</div>
-              <div className="stakeholderStats">
-                <div className="grid grid-cols-2">
-                  <Image src={badge} height={18} width={13}/>
-                  <div>00</div>
-                </div>
-                <div className="grid grid-cols-2">
-                  <Image src={projects} height={18} width={13}/>
-                  <div>00</div>
-                </div>
-                <div className="grid grid-cols-2 ">
-                  <Image src={donado} height={18} width={13}/>
-                  <div>00</div>
-                </div>
-              </div>
-            </div>
-            <div className="col-span-12 border-b"/>
-          </div>
-          <div className="memberGrid">
-            <div className="col-span-2 font-bold text-center">2</div>
-            <div className="stakeholderPhoto">
-              <Image src={blank} height={40} width={40}/>
-            </div>
-            <div className="stakeholderData">
-              <div className="stakeholderName">Address / ENS / Lens</div>
-              <div className="stakeholderStats">
-                <div className="grid grid-cols-2">
-                  <Image src={badge} height={18} width={13}/>
-                  <div>00</div>
-                </div>
-                <div className="grid grid-cols-2">
-                  <Image src={projects} height={18} width={13}/>
-                  <div>00</div>
-                </div>
-                <div className="grid grid-cols-2 ">
-                  <Image src={donado} height={18} width={13}/>
-                  <div>00</div>
-                </div>
-              </div>
-            </div>
-            <div className="col-span-12 border-b"/>
-          </div>
-          <div className="memberGrid">
-            <div className="col-span-2 font-bold text-center">3</div>
-            <div className="stakeholderPhoto">
-              <Image src={blank} height={40} width={40}/>
-            </div>
-            <div className="stakeholderData">
-              <div className="stakeholderName">Address / ENS / Lens</div>
-              <div className="stakeholderStats">
-                <div className="grid grid-cols-2">
-                  <Image src={badge} height={18} width={13}/>
-                  <div>00</div>
-                </div>
-                <div className="grid grid-cols-2">
-                  <Image src={projects} height={18} width={13}/>
-                  <div>00</div>
-                </div>
-                <div className="grid grid-cols-2 ">
-                  <Image src={donado} height={18} width={13}/>
-                  <div>00</div>
-                </div>
-              </div>
-            </div>
-            <div className="col-span-12 border-b"/>
-          </div>
-          <div className="memberGrid">
-            <div className="col-span-2 font-bold text-center">4</div>
             <div className="stakeholderPhoto">
               <Image src={blank} height={40} width={40}/>
             </div>
