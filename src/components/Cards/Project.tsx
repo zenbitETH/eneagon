80555c21e0716f8e47a580a710d4d81a02bf83b3
HabacucMX <habacuc.vera@gmail.com>
2022-09-19 14:13:57 -0500

    New project on dashboard

--- src/components/Cards/Project.tsx
+++ src/components/Cards/Project.tsx
@@ -2,29 +2,29 @@ export default function Project () {
     return (
         <div className="projects">
         <div className="projectGrid">
-          <div className="projectHeader">
-            <div className="col-span-8">
-              <div className="font-bold text-xl">
-                Project Tittle / Stage
-              </div>  
-              <div className="">
-                type of project / details
-              </div>
+            <div className="hover:bg-color1-500 bg-color2-500 rounded-gen font-bold text-xl md:w-1/4 hover:text-white text-center cursor-pointer py-2">+ New project</div>
+            <div className="projectHeader">
+                <div className="col-span-8">
+                  <div className="font-bold text-xl">
+                    Project Tittle / Stage
+                  </div>  
+                  <div className="">
+                    type of project / details
+                  </div>
+                </div>
+                <div className="col-span-4 text-sm text-right grid gap-1">
+                  <div>
+                    Updated 00 days agos
+                  </div> 
+                  <div>
+                    last tx 00 minutes ago
+                  </div>
+                </div>
             </div>
-            <div className="col-span-4 text-sm text-right grid gap-1">
-              <div>
-                Updated 00 days agos
-              </div> 
-              <div>
-                last tx 00 minutes ago
-              </div>
+            <div className="projectDescription">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
             </div>
-            
-          </div>
-          <div className="projectDescription">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </div>
-          <div className="projectStats">
+            <div className="projectStats">
               <div className="col-span-12 grid grid-cols-4">
                 <div className="projectText">
                   <div>•</div>
@@ -41,10 +41,9 @@ export default function Project () {
                 <div className="projectText">
                   <div className="projectNumbold">10</div>
                   <div className="col-span-5">w/dowloads</div>
-                </div>
-                
-              </div>
-              <div className="col-span-6 text-center grid grid-cols-3">
+                </div>    
+            </div>
+            <div className="col-span-6 text-center grid grid-cols-3">
                 <div>
                   web/demo
                 </div>
