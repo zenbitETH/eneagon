15caa71cbc1b6468f98698a54eb16f4095cd26c9
HabacucMX <habacuc.vera@gmail.com>
2022-09-21 00:30:22 -0500

    Icons on components

--- src/components/Cards/Projects.tsx
+++ src/components/Cards/Projects.tsx
@@ -1,39 +1,42 @@
 import zen from "../../assets/zenbit.png"
 import Image from "next/image"
 import Link from "next/link"
-import donado from "../../assets/donated.svg"
-import badge from '../../assets/Badges.svg'
+import donado from "../../assets/donatedW.svg"
+import badge from '../../assets/BadgesW.svg'
 
 export default function Project () {
     return (
         <div className="projects">
         <div className="projectGrid">
-
           <div className="projectView">
-            <div className="col-span-6">
-              <Image src={zen} width={150} height={150}/>
+            <div className="col-span-6 grid-cols-6 grid items-center">
+              <div className="col-span-2 ml-5"><Image src={zen} height={250} width={250}/></div>
+              <div className="grid col-span-4">
+                <div className="font-bold text-left text-color1-500 text-2xl px-5 ">
+                  Project Name <span className="mt-1 text-black text-base ml-3">ERC 1155</span>
+                  <div className="italic text-black text-base font-normal my-2">Type of project</div>
+                </div>
+                <div className="px-5 text-left mr-5">
+                  
+                  <div className="">🌱 <span className="italic">Stage 1</span></div>
+                </div>
+              </div>
             </div>
             <div className="projectInfo">
-              <div className="font-bold text-color1-500 text-2xl col-span-8 px-5 ">
-                Project Name
-                <div className="italic text-black text-base font-normal">Type of project</div>
-              </div>
-              <div className="col-span-4 text-right mr-5 ">
-                <div className="mt-1 text-color1-500 text-lg">ERC 1155</div>
-                <div className="">🌱 <span className="italic">Stage 1</span></div>
-              </div>
-              <div className="col-span-12 mx-5  my-2">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+              
+              <div className="col-span-12 mx-5  mb-2">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+
               </div>
               
-              <div className="col-span-12 grid grid-cols-2 bg-color1-500 rounded-b-gen text-white">
+              <div className="text-xl col-span-12 grid grid-cols-2 bg-color1-500 rounded-b-gen text-white">
                 <div className="grid grid-cols-2 p-1 items-center">
-                  <Image src={badge} height={18} width={13}/>
-                  <div>00</div>
+                  <Image src={badge} height={23} width={18}/>
+                  <div className="text-left">00</div>
                 </div>
-                <div className="grid grid-cols-2 p-1 items-center ">
-                  <Image src={donado} height={18} width={13}/>
-                  <div>00</div>
+                <div className="grid grid-cols-4 p-1 items-center ">
+                  <Image src={donado} height={23} width={18}/>
+                  <div className="text-left col-span-3">000.00 USD</div>
                 </div>
               </div>
             </div>
--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,6 +1,10 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import Medal from '../assets/Badges.svg'
+import Medal from '../assets/BadgesW.svg'
+import Book from '../assets/Repo.svg'
+import Member from '../assets/memberW.svg'
+import homebt from '../assets/home.svg'
+import newPro from '../assets/projectPlus.svg'
 
 export default function Toolbar() {
     return (
@@ -10,7 +14,17 @@ export default function Toolbar() {
         <Link href='/'>
             <a className='imageBG'>
               <Image
-                src={Medal}
+                src={homebt}
+                width={35}
+                height={35}
+                className="hover:regen-100 cursor-pointer"
+              />
+            </a>
+          </Link>
+          <Link href='/'>
+            <a className='imageBG'>
+              <Image
+                src={newPro}
                 width={35}
                 height={35}
                 className="hover:regen-100 cursor-pointer"
@@ -20,7 +34,7 @@ export default function Toolbar() {
           <Link href='/members'>
             <a className='imageBG'>
               <Image
-                src={Medal}
+                src={Member}
                 width={35}
                 height={35}
                 className="hover:regen-100 cursor-pointer"
@@ -40,7 +54,7 @@ export default function Toolbar() {
           <Link href='/'>
             <a className='imageBG'>
               <Image
-                src={Medal}
+                src={Book}
                 width={35}
                 height={35}
                 className="hover:regen-100 cursor-pointer"
