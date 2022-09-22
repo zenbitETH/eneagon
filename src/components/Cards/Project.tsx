56f11c1783f100a015a8254844c3e2ea0253ae92
HabacucMX <habacuc.vera@gmail.com>
2022-09-21 20:38:28 -0500

    @daphnehdz feedback

--- src/components/Cards/Project.tsx
+++ src/components/Cards/Project.tsx
@@ -1,24 +1,22 @@
 import zen from "../../assets/zenbit.png"
 import Image from "next/image"
-import donado from "../../assets/donatedW.svg"
-import badge from '../../assets/BadgesW.svg'
+import donado from "../../assets/donated.svg"
+import badge from '../../assets/Badges.svg'
 import Link from "next/link"
 
 export default function Project () {
     return (
       <div className="projectView">
         <div className="col-span-6 grid-cols-6 grid items-center relative">
-        <Link href="/projectDetail"><div className="absolute right-5 top-0 cursor-pointer hover:text-color1-500 ">+ Details</div></Link>
+        <div className="absolute right-5 top-0 cursor-pointer text-4xl">🌱</div>
           <div className="col-span-2 ml-5"><Image src={zen} height={250} width={250}/></div>
           <div className="grid col-span-4 grid-cols-12 grid">
-            <div className="col-span-8 text-left pl-5">
+            <div className="col-span-12 text-left pl-5">
               <div className="projectName">Project Name</div>
-              <div className="italic text-base font-normal ">Type of project</div>
+              <div className="italic text-base font-normal ">Type of project 
+                <span className="pl-5">ERC 1155</span>
+              </div>
             </div>
-          <div className="col-span-4 font-bold">
-            <div className="mt-1 text-base">ERC 1155</div>
-            <div className=" ">🌱 <span className="italic ">Stage 1</span></div>
-          </div>
           </div>
         </div>
         <div className="projectInfo">
@@ -34,6 +32,9 @@ export default function Project () {
               <Image src={donado} height={23} width={18}/>
               <div className="text-left col-span-3">000.00 USD</div>
             </div>
+            <Link href="/projectDetail">
+              <div className="text-color1-500 cursor-pointer hover:animate-pulse text-right pr-5">Show more ></div>
+            </Link>
           </div>
         </div>
       </div>
